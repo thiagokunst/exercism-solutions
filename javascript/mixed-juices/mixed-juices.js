3affbb93ca87fcfe11cbf97081f15f08e55d18b3
thiagokunst <61151858+thiagokunst@users.noreply.github.com>
2022-05-17 23:51:17 -0300

    mixed-juices solution

---
+++ javascript/mixed-juices/mixed-juices.js
@@ -0,0 +1,79 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Determines how long it takes to prepare a certain juice.
+ *
+ * @param {string} name
+ * @returns {number} time in minutes
+ */
+export function timeToMixJuice(name) {
+  switch(name){
+    case "Pure Strawberry Joy":
+      return 0.5;
+      break;
+    case "Energizer":
+      return 1.5;
+      break;
+    case "Green Garden":
+      return 1.5;
+      break;
+    case "Tropical Island":
+      return 3;
+      break;
+    case "All or Nothing":
+      return 5;
+      break;
+    default:
+      return 2.5;
+  }
+}
+
+/**
+ * Calculates the number of limes that need to be cut
+ * to reach a certain supply.
+ *
+ * @param {number} wedgesNeeded
+ * @param {string[]} limes
+ * @returns {number} number of limes cut
+ */
+export function limesToCut(wedgesNeeded, limes) {
+  let totalWedges = 0;
+  let limeCount = 0;
+  while(limes.length > 0 && totalWedges < wedgesNeeded){
+    switch(limes[0]){
+      case "small":
+        totalWedges += 6;
+        break;
+      case "medium":
+        totalWedges += 8;
+        break;
+      case "large":
+        totalWedges += 10;
+        break;
+      default: 
+      break;
+    }
+    limes.shift();
+    limeCount++
+  }
+  return limeCount;
+}
+
+/**
+ * Determines which juices still need to be prepared after the end of the shift.
+ *
+ * @param {number} timeLeft
+ * @param {string[]} orders
+ * @returns {string[]} remaining orders after the time is up
+ */
+export function remainingOrders(timeLeft, orders) {
+  do {
+      timeLeft -= timeToMixJuice(orders[0]);
+      orders.shift();
+  } while(orders.length > 0 && timeLeft > 0)
+  return orders;
+}
